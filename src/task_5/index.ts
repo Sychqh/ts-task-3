fdd06ec5afc788c76a421ac83ef2ed0c428f39e4
Sychqh <sychqh@gmail.com>
2021-11-08 19:55:08 +0500

    Сделал 5 задание

--- src/task_5/index.ts
+++ src/task_5/index.ts
@@ -7,17 +7,46 @@
  *
  * Хранилища должны быть сохранены в массив vaultStore: Vault[]
  */
-import { IContract } from "../task_4";
-import { ISecureVaultRequisites } from "../task_3";
+import {IContract} from "../task_4";
+import {ISecureVaultRequisites, Vault} from "../task_3";
 
-export class BankController{
-
-    public registerVault(): ISecureVaultRequisites{
-
+export class BankController {
+    private static instance: BankController;
+    private _vaultStore: Vault[];
+    
+    public static getInstance(): BankController {
+        if (!this.instance) {
+            this.instance = new BankController();
+        }
+        
+        return this.instance;
     }
-
-    public proceedContract(contract: IContract) {
-
+    
+    public registerVault(): ISecureVaultRequisites {
+        const newVault = new Vault();
+        this._vaultStore.push(newVault);
+        
+        return newVault;
+    }
+    
+    public proceedContract(contract: IContract): void {
+        const sender = this._vaultStore.find((vault: Vault) => vault.id === contract.id);
+        const receiver = this._vaultStore.find((vault: Vault) => vault.id === contract.receiver.id);
+        if (sender === undefined || receiver === undefined) {
+            throw new Error("Sender or receiver don't exist")
+        }
+        
+        contract.signAndTransfer();
+        try {
+            sender.withdraw(contract.value);
+        } catch (e) {
+            contract.closeTransfer();
+            
+            return;
+        }
+        
+        receiver.deposit(contract.value);
+        contract.closeTransfer;
     }
 }
 
